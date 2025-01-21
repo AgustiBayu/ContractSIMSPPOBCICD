005febe72a1b99d8038c4d22486650ff6f9b571d
AgustiBayu <agustibayu.abs@gmail.com>
2025-01-21 17:33:47 +0700

    config database

---
+++ app/database.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"ContractSIMSPPOBCICD/helper"
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func NewDB() *sql.DB {
+	err := godotenv.Load()
+	helper.PanicIfError(err)
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbSslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", dbUser, dbPass, dbHost, dbName, dbSslmode)
+	db, err := sql.Open("postgres", dsn)
+	helper.PanicIfError(err)
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
