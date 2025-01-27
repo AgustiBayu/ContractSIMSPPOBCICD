e2a3110c51f5ad168883b1c157b0218e06517936
AgustiBayu <agustibayu.abs@gmail.com>
2025-01-27 16:22:08 +0700

    update workflows dan implement unit testing v9

--- app/database_test.go
+++ app/database_test.go
@@ -1,15 +1,19 @@
 package app
 
 import (
+	"ContractSIMSPPOBCICD/helper"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewDB(t *testing.T) {
+	err := godotenv.Load("../.env")
+	helper.PanicIfError(err)
 	db := NewDB()
 	assert.NotNil(t, db, "database is null")
-	err := db.Ping()
+	err = db.Ping()
 	assert.NoError(t, err, "Database connection should be successful")
 	defer func() {
 		err := db.Close()
