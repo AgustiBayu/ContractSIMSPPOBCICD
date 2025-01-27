7f834266bbf27020d5abefa57c54f3fd384a0d7d
AgustiBayu <agustibayu.abs@gmail.com>
2025-01-27 21:34:36 +0700

    update workflows dan implement unit testing v15

--- app/database_test.go
+++ app/database_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"os"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -8,13 +9,20 @@ import (
 )
 
 func TestNewDB(t *testing.T) {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		t.Fatal("Error loading .env file")
+	// Coba load file .env jika ada di direktori yang lebih tinggi
+	if _, err := os.Stat("../.env"); err == nil {
+		// Jika file ada, load file .env
+		err := godotenv.Load("../.env")
+		if err != nil {
+			t.Fatal("Error loading .env file")
+		}
+	} else {
+		// Jika file .env tidak ditemukan, log pesan tetapi lanjutkan untuk menggunakan environment variables dari sistem
+		t.Log("Warning: .env file not found, using system environment variables")
 	}
 	db := NewDB()
 	assert.NotNil(t, db, "database is null")
-	err = db.Ping()
+	err := db.Ping()
 	assert.NoError(t, err, "Database connection should be successful")
 	defer func() {
 		err := db.Close()
