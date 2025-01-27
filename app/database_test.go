2689c836656c89a70fecfaab6fc74cbdbd3fa186
AgustiBayu <agustibayu.abs@gmail.com>
2025-01-27 22:01:12 +0700

    update workflows dan implement unit testing v16

--- app/database_test.go
+++ app/database_test.go
@@ -1,25 +1,12 @@
 package app
 
 import (
-	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewDB(t *testing.T) {
-	// Coba load file .env jika ada di direktori yang lebih tinggi
-	if _, err := os.Stat("../.env"); err == nil {
-		// Jika file ada, load file .env
-		err := godotenv.Load("../.env")
-		if err != nil {
-			t.Fatal("Error loading .env file")
-		}
-	} else {
-		// Jika file .env tidak ditemukan, log pesan tetapi lanjutkan untuk menggunakan environment variables dari sistem
-		t.Log("Warning: .env file not found, using system environment variables")
-	}
 	db := NewDB()
 	assert.NotNil(t, db, "database is null")
 	err := db.Ping()
